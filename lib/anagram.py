1d6b849cf4e9ba12f4cccb1827a70b73ec78f307
itsgichia <gichiamuiruri@gmail.com>
2024-03-08 23:04:50 +0300

    task completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+# your code goes here
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            if sorted(word) == sorted(self.word) and word != self.word:
+                matches.append(word)
+        return matches
